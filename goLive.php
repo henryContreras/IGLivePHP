09544ba043c752bfa7ee88f977ddf58fe4a192d1
JRoy <joshroy126@gmail.com>
2019-09-19 15:49:29 -0400

    Stable Release 60

--- goLive.php
+++ goLive.php
@@ -10,8 +10,8 @@ if (php_sapi_name() !== "cli") {
 
 //Script version constants
 define("scriptVersion", "1.9");
-define("scriptVersionCode", "59");
-define("scriptFlavor", "beta");
+define("scriptVersionCode", "60");
+define("scriptFlavor", "stable");
 
 //Command Line Argument Registration
 $helpData = [];
